bce977a92c6d55d34d8e898ae4abf8f2c5309d7d
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-02-13 13:24:12 +0000

    combine description strings and set default feature values methods created

--- tabular_data.py
+++ tabular_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 
 file="/Users/gebruiker/modelling-airbnbs-property-listing-dataset-/airbnb-property-listings/tabular_data/listing.csv"
@@ -11,14 +12,39 @@ def remove_rows_with_missing_ratings(df):
     location_nulls = df['Location_rating'].isnull()
     check_nulls = df['Check-in_rating'].isnull()
     value_nulls = df['Value_rating'].isnull()
+    desc_null = df['Description'].isnull()
     df =(df[~clean_nulls])
     df=(df[~accu_nulls])
     df=(df[~comm_nulls])
     df=(df[~location_nulls])
     df=(df[~check_nulls])
     df=(df[~value_nulls])
+    df = df[~desc_null]
     return df
 
 new_df = remove_rows_with_missing_ratings(df)
-print(new_df)
+
+def combine_description_strings(new_df):
+    new_df['Description'] = new_df['Description'].apply(lambda x: x.replace("'About this space',",''))
+    new_df['Description'] = new_df['Description'].apply(lambda x: x.replace("'",'"'))
+    new_df['Description'] = new_df['Description'].apply(lambda x: x.replace('"',''))
+    new_df['Description'] = new_df['Description'].apply(lambda x: x.replace(",",''))
+    new_df['Description'] = new_df['Description'].apply(lambda x: "".join(x))
+    return new_df
+
+def set_default_feature_values(new_df):
+    new_df[['guests', 'beds', 'bathrooms','bedrooms']] = new_df[['guests', 'beds', 'bathrooms','bedrooms']].fillna(value=1)
+    print((new_df.head()))
+    return new_df
+
+set_default_feature_values(new_df)
+
+
+
+
+#new_df['Description'] = new_df['Description'].str.join("\n")
+
+
+
+
 
