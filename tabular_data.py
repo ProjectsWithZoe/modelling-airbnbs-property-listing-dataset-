3add673b83e1b080ae81ab755313cf30db71fdb5
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-02-20 18:39:23 +0000

    Combine description strings method solved

--- tabular_data.py
+++ tabular_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from ast import literal_eval
-file="/Users/gebruiker/modelling-airbnbs-property-listing-dataset-/airbnb-property-listings/tabular_data/listing.csv"
-raw_df = pd.read_csv(file)
+#file="/Users/gebruiker/modelling-airbnbs-property-listing-dataset-/airbnb-property-listings/tabular_data/listing.csv"
+#raw_df = pd.read_csv(file)
 def remove_rows_with_missing_ratings(df):
     df = df.dropna(subset=['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating', 'Check-in_rating', 'Value_rating'])
     return df
@@ -18,20 +18,14 @@ def combine_description_strings(new_df):
     new_df = new_df.dropna(subset=['Description'])
     new_df = new_df.copy()
     new_df['Description'] = new_df['Description'].apply(lambda x: x.replace("'About this space',",''))
-    #new_df['Description'] = new_df['Description'].apply(lambda x: x.replace("'",'"'))
-    #new_df['Description'] = new_df['Description'].apply(lambda x: x.replace('"',''))
-    #new_df['Description'] = new_df['Description'].apply(lambda x: x.replace(",",''))
-    #new_df['Description'] = new_df['Description'].apply(lambda x: "".join(x))
+    new_df['Description'] = new_df['Description'].apply(lambda x: x.replace(",",''))
     new_df['Description'] = new_df['Description'].apply(lambda x: literal_return(x))
+    new_df['Description']=new_df['Description'].str.join(",")
     return new_df
-    
-    #print(new_df.head())
-    #return new_df
 
 def set_default_feature_values(new_df):
     new_df[['guests', 'beds', 'bathrooms','bedrooms']] = new_df[['guests', 'beds', 'bathrooms','bedrooms']].fillna(value=1)
-    print(new_df.isnull().sum())
-    #print((new_df.head()))
+    #print(new_df.isnull().sum())
     return new_df
 
 def clean_tabular_data(df):
@@ -40,8 +34,18 @@ def clean_tabular_data(df):
     df = set_default_feature_values(df)
     return df
 
-df = combine_description_strings(new_df=remove_rows_with_missing_ratings(raw_df))
-print(type(df['Description'][0]))
+def load_airbnb(label):
+    cleaned_df = clean_tabular_data(raw_df)
+    nums = ['float64', 'int64']
+    cleaned_df = cleaned_df.select_dtypes(include=nums)
+    labels = cleaned_df[label]
+    features = cleaned_df.drop([label], axis =1)
+    #print (features, labels)
+    return (features, labels)
+    
+
+#df = combine_description_strings(new_df=remove_rows_with_missing_ratings(raw_df))
+#print((df['Description'][1]))
 
 
 if __name__ == '__main__':
