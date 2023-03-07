f9019dc942e573711c7cfb82763bb644adb3fc90
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-03-07 22:03:07 +0000

    RMSE and R2 values computed

--- modelling.py
+++ modelling.py
@@ -1,9 +1,10 @@
 import tabular_data
 import pandas as pd
+import numpy as np
 
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -23,6 +24,21 @@ reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty='l2', random_state=42)
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
-print('Mean squared error:', mse);
+#print('Mean squared error:', mse);
 #plot=sns.scatterplot(x=X, y=y)
 #print(plot)
+
+#predictions for training and test sets
+y_train_pred = reg.predict(X_train)
+y_test_pred = reg.predict(X_test)
+
+#print(y_train_pred, y_test_pred)
+
+rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
+rmse_test = np.sqrt(mean_squared_error(y_test, y_test_pred))
+
+r2_train = r2_score(y_train, y_train_pred)
+r2_test = r2_score(y_test, y_test_pred)
+
+print(f'Rmse : {rmse_test}, {rmse_train}')
+print(f'R2 : {r2_test}, {r2_train}')
