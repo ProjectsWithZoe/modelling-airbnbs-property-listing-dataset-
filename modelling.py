a1840488cb4d1ce755fe40210d18035a05dbebd7
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-03-14 09:56:52 +0000

    X data scaled

--- modelling.py
+++ modelling.py
@@ -2,6 +2,7 @@ import tabular_data
 import pandas as pd
 import numpy as np
 
+from sklearn.preprocessing import scale
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
@@ -14,33 +15,39 @@ raw_df = pd.read_csv(file)
 
 df= tabular_data.clean_tabular_data(df=raw_df)
 tup = tabular_data.load_airbnb(df,label='Price_Night')
-X = tup[1] #features
-y = tup[0] #labels
+#X = tup[1] #features
+#y = tup[0] #labels
 nums = ['float64', 'int64'] 
-X = X.select_dtypes(include = nums)
+X_nums = tup[1].select_dtypes(include = nums)
 
+def scale_data(features, labels):
+    #converts features and labels to np array and applies scaling
 
+    X = np.array(X_nums)
+    y = np.array(tup[0])
+    X = scale(X)
+
+    return X,y
+X,y = scale_data(X_nums, tup[0])
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
 model = SGDRegressor(tol=1e-3, penalty='l2', random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
-#print('Mean squared error:', mse);
-#plot=sns.scatterplot(x=X, y=y)
-#print(plot)
+#print('Mean squared error:', mse)
 
 #predictions for training and test sets
-y_train_pred = model.predict(X_train)
-y_test_pred = model.predict(X_test)
+#y_train_pred = model.predict(X_train)
+#y_test_pred = model.predict(X_test)
 
 #print(y_train_pred, y_test_pred)
 
-rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
-rmse_test = np.sqrt(mean_squared_error(y_test, y_test_pred))
+#rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
+#rmse_test = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
-r2_train = r2_score(y_train, y_train_pred)
-r2_test = r2_score(y_test, y_test_pred)
+#r2_train = r2_score(y_train, y_train_pred)
+#r2_test = r2_score(y_test, y_test_pred)
 
 #print(model.get_params())
 
@@ -50,6 +57,8 @@ hyperparameters = {
     'alpha': [0.001,0.1,1,10],
     'max_iter':[10,100,200,500,1000, 10000, 100000, 1000000]
     }
+
+
 def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, X_val, y_val, X_test, y_test, hyperparameters):
     best_score = 0
     best_params = {}
@@ -64,26 +73,10 @@ def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, 
         model.fit(X_train, y_train)
         score = model.score(X_val,y_val)
         y_val_pred = model.predict(X_val)
-
-        #rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
-        rmse_val = np.sqrt(mean_squared_error(y_val, y_val_pred))
-        #r2_train = r2_score(y_train_pred, y_train)
+        
         r2_val = r2_score(y_val, y_val_pred)
-        #print(rmse_train)
-        #print(rmse_val)
-        #print (rmse_val, r2_val) 
-        if score > best_score:
-            best_score = score
-            best_params = params
-
-    print(score, best_params)
-
-        #if r2_val > best_r2:
-            #best_r2 = r2_val
-            #best_params = params
-
-    #print(best_r2, best_params)
-    #return best_score, best_params
+      
+        print(score)
 
 
 custom_tune_regression_model_hyperparameters(model_class=SGDRegressor, X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test, hyperparameters=hyperparameters)
