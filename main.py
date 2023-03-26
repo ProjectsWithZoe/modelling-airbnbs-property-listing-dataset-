f334067aba84b4ed66dff24d9bc5739eb052a702
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-03-26 22:29:13 +0100

    Category loaded from airbnb

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import tabular_data, modelling
+import pandas as pd
+file="/Users/gebruiker/modelling-airbnbs-property-listing-dataset-/airbnb-property-listings/tabular_data/listing.csv"
+raw_df = pd.read_csv(file)
+df = tabular_data.clean_tabular_data(raw_df)
+
+labels, features = tabular_data.load_airbnb(df, 'Category')
+print(labels.unique())
 
