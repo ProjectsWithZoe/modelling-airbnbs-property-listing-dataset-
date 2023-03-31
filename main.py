3c15b538ec9e8d3f52111a1d71a01df13acda482
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-03-31 10:51:22 +0100

    TP,TN,FP,FN functions created for metrics calculations

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import scale
-from sklearn.linear_model import SGDRegressor
-from sklearn.model_selection import GridSearchCV
+#from sklearn.linear_model import SGDRegressor
+#from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix
-from sklearn.datasets import make_regression
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn import svm
+from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, accuracy_score, precision_score, recall_score
+#from sklearn.datasets import make_regression
+from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, 
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn import svm
 import matplotlib.pyplot as plt
-import seaborn as sns
-import itertools
-import os
-import json
-from joblib import dump
+#import seaborn as sns
+#import itertools
+#import os
+#import json
+#from joblib import dump
 
 
 
@@ -48,8 +50,48 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.2, random_
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 dtr_model = DecisionTreeClassifier()
 dtr_model.fit(X_train, y_train)
-dtr_predictions = dtr_model.predict(X_test)
-score = dtr_model.score(X_val, y_val)
+dtr_pred = dtr_model.predict(X_test)
+train_pred = dtr_model.predict(X_train)
+dtr_score = dtr_model.score(X_val, y_val)
+#print(dtr_score)
+#dtr_cm = confusion_matrix(y_test, dtr_predictions)
+#d_cmd = ConfusionMatrixDisplay(dtr_cm)
+#d_cmd.plot()
+#plt.show()
+'''Metrics'''
+accuracy_test = accuracy_score(y_test, dtr_pred)
+accuracy_train = accuracy_score(y_train, train_pred)
+#print(accuracy_test, accuracy_train)
 
-print(dtr_predictions)
-print(score)
+def true_positive(labels, targets):
+    return np.sum(labels.astype(bool) & targets.astype(bool))
+def true_negative(labels, targets):
+    return np.sum(~labels.astype(bool) & ~targets.astype(bool))
+def false_positive(labels, targets):
+    return np.sum(~labels.astype(bool) & targets.astype(bool))
+def false_negative(labels, targets):
+    return np.sum(labels.astype(bool) & ~targets.astype(bool))
+
+print(true_positive(y_test, dtr_pred))
+print(true_negative(y_test, dtr_pred))
+print(false_positive(y_test, dtr_pred))
+print(false_negative(y_test, dtr_pred))
+#recall_test = recall_score(y_test, dtr_pred)
+#recall_train = recall_score(y_train, train_pred)
+#print(recall_test, recall_train)
+#accuracy_train = accuracy_score(y)
+#recall = recall_score(y_test, )
+
+#rfc_model = RandomForestClassifier()
+#rfc_model.fit(X_train, y_train)
+#rfc_predictions = rfc_model.predict(X_test)
+#rfc_score = rfc_model.score(X_val, y_val)
+#print(rfc_predictions)
+#print(rfc_score)
+
+#knn_model = KNeighborsClassifier()
+#knn_model.fit(X_train, y_train)
+#knn_predictions = knn_model.predict(X_test)
+#knn_score = knn_model.score(X_val, y_val)
+#print(knn_predictions)
+#print(knn_score)
